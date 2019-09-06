18917a1e23dd2a46aa25e8533f8b4c11eb7baed2
Eric Löffler <eric.loeffler@opalia.systems>
2019-09-06 04:29:15 +0200

    add method to filter OSGi services

--- src/main/java/systems/opalia/interfaces/soa/osgi/ServiceManager.java
+++ src/main/java/systems/opalia/interfaces/soa/osgi/ServiceManager.java
@@ -1,10 +1,9 @@
 package systems.opalia.interfaces.soa.osgi;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 
@@ -35,6 +34,20 @@ public class ServiceManager {
                     return Optional.ofNullable(bundleContext.getService(x));
                 })
                 .orElseThrow(() -> new IllegalArgumentException("Cannot find service " + clazz.getName() + "."));
+    }
+
+    public <T> List<T> getServices(BundleContext bundleContext, Class<T> clazz, String filter)
+            throws InvalidSyntaxException {
+
+        return bundleContext.getServiceReferences(clazz, filter).stream()
+                .map((x) -> {
+
+                    serviceReferences.add(x);
+
+                    return bundleContext.getService(x);
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     public void unregisterServices() {
