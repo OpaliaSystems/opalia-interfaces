OpaliaSystems/opalia-commons

create a pattern for ID classes

src/main/scala/systems/opalia/commons/identifier/Identifier.scala
@@ -1,0 +1,26 @@
+package systems.opalia.commons.identifier
+
+import java.util.Objects
+
+
+trait Identifier
+  extends IndexedSeq[Byte] {
+
+  protected val id: Vector[Byte]
+
+  override def apply(index: Int): Byte =
+    id(index)
+
+  override def length: Int =
+    id.length
+
+  override def equals(that: Any): Boolean =
+    that match {
+
+      case that: Identifier if (this.getClass == that.getClass && this.sameElements(that)) => true
+      case _ => false
+    }
+
+  override def hashCode: Int =
+    Objects.hash(id.map(Byte.box): _*)
+}

src/main/scala/systems/opalia/commons/identifier/IdentifierCompanion.scala
@@ -1,0 +1,21 @@
+package systems.opalia.commons.identifier
+
+
+trait IdentifierCompanion[T <: Identifier] {
+
+  def isValid(that: String): Boolean
+
+  def isValid(that: Seq[Byte]): Boolean
+
+  def length: Int
+
+  def getNew: T
+
+  def getFrom(that: String): T
+
+  def getFrom(that: Seq[Byte]): T
+
+  def getFromOpt(that: String): Option[T]
+
+  def getFromOpt(that: Seq[Byte]): Option[T]
+}

OpaliaSystems/opalia-interfaces

src/main/scala/systems/opalia/interfaces/identifier/Identifier.scala
@@ -1,0 +1,26 @@
+package systems.opalia.interfaces.identifier
+
+import java.util.Objects
+
+
+trait Identifier
+  extends IndexedSeq[Byte] {
+
+  protected val data: Vector[Byte]
+
+  override def apply(index: Int): Byte =
+    data(index)
+
+  override def length: Int =
+    data.length
+
+  override def equals(that: Any): Boolean =
+    that match {
+
+      case that: Identifier if (this.getClass == that.getClass && this.sameElements(that)) => true
+      case _ => false
+    }
+
+  override def hashCode: Int =
+    Objects.hash(data.map(Byte.box): _*)
+}

src/main/scala/systems/opalia/interfaces/identifier/IdentifierCompanion.scala
@@ -1,0 +1,21 @@
+package systems.opalia.interfaces.identifier
+
+
+trait IdentifierCompanion[T <: Identifier] {
+
+  def isValid(that: String): Boolean
+
+  def isValid(that: Seq[Byte]): Boolean
+
+  def length: Int
+
+  def getNew: T
+
+  def getFrom(that: String): T
+
+  def getFrom(that: Seq[Byte]): T
+
+  def getFromOpt(that: String): Option[T]
+
+  def getFromOpt(that: Seq[Byte]): Option[T]
+}